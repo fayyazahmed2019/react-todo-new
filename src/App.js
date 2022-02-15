c921b8b5071c9424f2f04e6387a41f4e99a8a368
fayyazahmed2019 <fayyaz.amyani@gmail.com>
2022-02-15 18:14:37 +0500

    React Todo New

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./App.css";
+import TodoForm from "./Components/TodoForm";
+
+
+const Todo=({ todo, index, completeTodo, removeTodo })=> {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>x</button>
+      </div>
+    </div>
+  );
+}
+
+
+<TodoForm />
+
+
+function App() {
+  const [todos, setTodos] = React.useState([
+
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
